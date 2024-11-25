e68e712374bc9faad7177ed581e32df85b5ff92b
Rat <rat@thebigrat.gay>
2024-11-25 00:26:35 -0800

    Added string representations and hash maps for instructions and their codes

--- ratsembler_6502/src/lang/ast.rs
+++ ratsembler_6502/src/lang/ast.rs
@@ -1,5 +1,8 @@
 use super::instruction::InstructionCode;
+use super::instruction::AddressModeIndexer;
 use std::vec::Vec;
+
+use pest::iterators::Pairs;
 
 pub enum ShortOperand<'a> {
     Numeric(u8),
@@ -38,4 +41,31 @@ pub struct Expression<'a> {
 pub struct Program<'program> {
     expressions: Vec<Expression<'program>>,
     labels: Vec<Box<str>>,
+}
+
+impl AddressValue<'_> {
+    pub fn to_indexer(self) -> AddressModeIndexer {
+        match self {
+            AddressValue::Accumulator => AddressModeIndexer::ACCUMULATOR,
+            AddressValue::Implied => AddressModeIndexer::IMPLIED,
+            AddressValue::Immediate(ShortOperand::Numeric(_)) => AddressModeIndexer::IMMEDIATE,
+            AddressValue::Immediate(ShortOperand::Label(_)) => AddressModeIndexer::IMMEDIATE,
+            AddressValue::Absolute(_) => AddressModeIndexer::ABSOLUTE,
+            AddressValue::ZeroPage(_) => AddressModeIndexer::ZERO_PAGE,
+            AddressValue::Relative(_) => AddressModeIndexer::RELATIVE,
+            AddressValue::AbsoluteIndirect(_) => AddressModeIndexer::ABSOLUTE_INDIRECT,
+            AddressValue::AbsoluteX(_) => AddressModeIndexer::ABS_X,
+            AddressValue::AbsoluteY(_) => AddressModeIndexer::ABS_Y,
+            AddressValue::ZeroPageX(_) => AddressModeIndexer::ZP_X,
+            AddressValue::ZeroPageY(_) => AddressModeIndexer::ZP_Y,
+            AddressValue::IndexedIndirect(_) => AddressModeIndexer::INDEX_IND,
+            AddressValue::IndirectIndexed(_) => AddressModeIndexer::IND_INDEX,
+        }
+    }
+}
+
+impl<'program> Program<'program> {
+    fn from_pairs(pairs: Pairs<'program, super::parser::Rule>) -> Program<'program> {
+        unimplemented!();
+    }
 }
